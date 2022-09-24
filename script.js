e3ba0bb10a6598d4b0ca53df20d967ede7cd0271
Bedi06 <113227370+Bedi06@users.noreply.github.com>
2022-09-24 15:19:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.getElementById('button').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
-  let new_ul = document.createElement('ul');
-  new_ul.innerText = 'Apple';
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
 
-  element_list.appendChild(new_ul);
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(getNumberOrString(document.getElementById('text').value)));
 
 });
